1037204a823bf102c7f986d051888013c878b46a
Fernando Marichal <contacto@fernandomarichal.com>
2025-04-16 20:49:01 -0300

    fix details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './features/todo-list/components/todo-list/todo-list.component';
-import { TodoItemComponent } from './features/todo-list/components/todo-item/todo-item.component';
-import { ConfirmationDialogComponent } from './features/todo-list/components/confirmation-dialog/confirmation-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoListModule } from './features/todo-list/todo-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoItemComponent,
-    ConfirmationDialogComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
-    MatDialogModule,
-    MatButtonModule
+    TodoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
